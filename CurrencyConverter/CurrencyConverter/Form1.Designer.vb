b38ae3d5223bca43395739fe07bb1b715d1d74b2
HermantNET <hermant.edu@gmail.com>
2016-04-03 00:57:47 -0400

    CurrencyCalculator
    Added json enabled currency calculator program, WinForms

---
+++ CurrencyConverter/CurrencyConverter/Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtEuro = New System.Windows.Forms.TextBox()
+        Me.txtPounds = New System.Windows.Forms.TextBox()
+        Me.txtYen = New System.Windows.Forms.TextBox()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(48, 227)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "&Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtEuro
+        '
+        Me.txtEuro.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.txtEuro.Location = New System.Drawing.Point(67, 12)
+        Me.txtEuro.Name = "txtEuro"
+        Me.txtEuro.ReadOnly = True
+        Me.txtEuro.Size = New System.Drawing.Size(100, 20)
+        Me.txtEuro.TabIndex = 1
+        '
+        'txtPounds
+        '
+        Me.txtPounds.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.txtPounds.Location = New System.Drawing.Point(67, 38)
+        Me.txtPounds.Name = "txtPounds"
+        Me.txtPounds.ReadOnly = True
+        Me.txtPounds.Size = New System.Drawing.Size(100, 20)
+        Me.txtPounds.TabIndex = 2
+        '
+        'txtYen
+        '
+        Me.txtYen.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.txtYen.Location = New System.Drawing.Point(67, 64)
+        Me.txtYen.Name = "txtYen"
+        Me.txtYen.ReadOnly = True
+        Me.txtYen.Size = New System.Drawing.Size(100, 20)
+        Me.txtYen.TabIndex = 3
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Location = New System.Drawing.Point(48, 201)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(100, 20)
+        Me.txtAmount.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "EUR"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "GBP"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "JPY"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 204)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "$"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 261)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(179, 22)
+        Me.StatusStrip1.TabIndex = 9
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.AutoSize = False
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(150, 17)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(179, 283)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.txtYen)
+        Me.Controls.Add(Me.txtPounds)
+        Me.Controls.Add(Me.txtEuro)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "CurCalc"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtEuro As TextBox
+    Friend WithEvents txtPounds As TextBox
+    Friend WithEvents txtYen As TextBox
+    Friend WithEvents txtAmount As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+End Class
---
+++ CurrencyConverter/CurrencyConverter/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports Newtonsoft.Json
+Imports System.Net
+
+Public Class Form1
+    Dim json = New WebClient().DownloadString("http://api.fixer.io/latest?base=USD")
+
+    Dim DataSet = JsonConvert.DeserializeObject(json)
+    Dim dataTable = DataSet("rates")
+
+    Sub Convert()
+        Try
+            Dim decAmount As Decimal = CDec(txtAmount.Text)
+            Dim decEuro As Decimal = decAmount * dataTable("EUR")
+            Dim decPounds As Decimal = decAmount * dataTable("GBP")
+            Dim decYen As Decimal = decAmount * dataTable("JPY")
+
+            txtEuro.Text = decEuro.ToString()
+            txtPounds.Text = decPounds.ToString()
+            txtYen.Text = decYen.ToString()
+        Catch
+            ToolStripStatusLabel1.Text = "Invalid Number"
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ToolStripStatusLabel1.Text = String.Empty
+        Convert()
+    End Sub
+End Class
