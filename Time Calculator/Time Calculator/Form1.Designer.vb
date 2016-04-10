bae0ce8f4b9f4673e205de2d9d21deb820605a99
HermantNET <hermant.edu@gmail.com>
2016-04-10 11:02:05 -0400

    TimeCalculator
    Added TimeCalculator program to Projects

---
+++ Time Calculator/Time Calculator/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSeconds = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(122, 91)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Seconds:"
+        '
+        'txtSeconds
+        '
+        Me.txtSeconds.Location = New System.Drawing.Point(111, 107)
+        Me.txtSeconds.Name = "txtSeconds"
+        Me.txtSeconds.Size = New System.Drawing.Size(103, 20)
+        Me.txtSeconds.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(111, 133)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(103, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Button1"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(12, 47)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(311, 27)
+        Me.lblResult.TabIndex = 3
+        Me.lblResult.Text = ":D"
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(311, 38)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Time Calculator"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 168)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtSeconds)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSeconds As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblResult As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Time Calculator/Time Calculator/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Time
+        Dim intTime As Integer
+        Dim blnInput As Boolean = True
+        Dim intMinutes As Integer = 0
+        Dim intSeconds As Integer = 0
+        Dim intHours As Integer = 0
+        Dim intDays As Integer = 0
+
+        lblResult.Text = String.Empty
+        If Integer.TryParse(txtSeconds.Text, intTime) = False Then
+            lblResult.Text = "Please Enter a number."
+            blnInput = False
+
+        Else
+
+            If blnInput = True Then
+
+                If intTime >= 86400 Then
+                    intDays = intTime \ 86400
+                    intTime = intTime Mod 86400
+                End If
+
+                If intTime >= 3600 Then
+                    intHours = intTime \ 3600
+                    intTime = intTime Mod 3600
+                End If
+
+                If intTime >= 60 Then
+                    intMinutes = intTime \ 60
+                    intTime = intTime Mod 60
+                End If
+            End If
+
+
+            lblResult.Text = intDays.ToString("n0") & " Days, " &
+            intHours.ToString("n0") & " hours, " &
+            intMinutes.ToString("n0") & " minutes, and " &
+            intTime.ToString("n0") & " seconds."
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblResult.Text = String.Empty
+    End Sub
+End Class
